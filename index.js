2affbd37ae316f3e4bd6e1df2d7ecbdea6259821
Moksina-Akter <msdiya01865@gmail.com>
2025-09-07 20:47:48 +0600

    all card added

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
+
+
 const catagoryContainer = document.getElementById('catagoryContainer');
 const plantCardContainer = document.getElementById('card-container');
+
+//alltrees
+const allTrees = () =>{
+     fetch('https://openapi.programming-hero.com/api/plants')
+    .then((response) => response.json())
+    .then((data) => {
+        // console.log(data);
+        // const categories = data.categories;
+        showAllTrees(data.plants) ;   
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+//showAllTrees
+
+const showAllTrees = (allTrees)=>{
+    //   console.log(allTrees);
+      plantCardContainer.innerHTML ='';
+      allTrees.forEach( tree => {
+       plantCardContainer.innerHTML += `
+           <div class="p-4 space-y-2 bg-white rounded-md shadow-lg">
+                 <div class="h-[220px]">
+                    <img class="w-full h-full rounded " src="${tree.image}" alt="">
+                 </div>
+                 <h1 class="text-lg font-semibold">${tree.name}</h1>
+                 <p>${tree.description}</p>
+                <div class="flex justify-between items-center">
+                    <h2 class="bg-[#dcfce7] text-[#15803d] py-1 px-2 rounded-2xl">${tree.category}</h2>
+                   <h3>৳<span class="text-[#15803d]">${tree.price}</span></h3>
+               </div>
+              <button class="bg-[#15803d] w-full py-1 px-2 rounded-3xl text-white">Add to Cart</button>      
+             </div>
+        `;
+   })
+  
+}
 
 //loadCategories-part
 const loadCategories = () => {
@@ -19,7 +59,7 @@ const showCategory = (categories) => {
         categories.forEach(category => {
             catagoryContainer.innerHTML += ` 
              
-                <li id="${category.id}" class="hover:bg-[#8ad3a7] p-1 rounded hover:text-[#ffffff] cursor-pointer text-[#1f2937]">${category.category_name}</li>
+                <li id="${category.id}" class="hover:bg-[#0ee15c] p-1 rounded hover:text-[#ffffff] cursor-pointer text-[#1f2937]">${category.category_name}</li>
             `;
             
         });
@@ -33,40 +73,40 @@ const showCategory = (categories) => {
                     //console.log(event.target);
                     event.target.classList.add('bg-[#15803d]')
                     event.target.classList.add('text-[#ffffff]')
+                    plantCategory(event.target.id)
                 }
             })
 }
 
 // plantCategory - part
-const plantCategory = (id) =>{
-      fetch(`https://openapi.programming-hero.com/api/category/${id}`)
-    //  fetch(`https://openapi.programming-hero.com/api/plants`)
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data);
-        const plantsCategories = data.plants;
+const plantCategory = (cardId) =>{
+      fetch(`https://openapi.programming-hero.com/api/category/${cardId}`)
+      .then((res) => res.json())
+      .then((data) => {
+         const plantsCategories = data.plants;
          showPlantCategory(plantsCategories) 
-    })
-    .catch(err => {
-        console.log(err);
+           })
+      .catch(err => {
+         console.log(err);
     })
 }
 
-//showPlantCategory - part
+
+// showPlantCategory - part
 
  const showPlantCategory = (plants) => {
     plantCardContainer.innerHTML = "";
     plants.forEach( plant => {
         plantCardContainer.innerHTML += `
-           <div class="p-4 space-y-2 bg-white rounded-md">
-                 <div>
-                    <img class="w-full h-full" src="${plant.image}" alt="">
+           <div class="p-4 space-y-2 bg-white rounded-md shadow-lg">
+                 <div class="h-[220px]">
+                    <img class="w-full h-full rounded " src="${plant.image}" alt="">
                  </div>
-                 <h1 class="text-base font-semibold">${plant.name}</h1>
+                 <h1 class="text-lg font-semibold">${plant.name}</h1>
                  <p>${plant.description}</p>
                 <div class="flex justify-between items-center">
                     <h2 class="bg-[#dcfce7] text-[#15803d] py-1 px-2 rounded-2xl">${plant.category}</h2>
-                   <h3><span>৳</span>${plant.price}</h3>
+                   <h3>৳<span class="text-[#15803d]">${plant.price}</span></h3>
                </div>
               <button class="bg-[#15803d] w-full py-1 px-2 rounded-3xl text-white">Add to Cart</button>      
              </div>
@@ -76,5 +116,7 @@ const plantCategory = (id) =>{
 
 
 loadCategories();
-plantCategory(1);
-//  plantCategory();
+allTrees()
+
+
+
