6405408c23758d34f7b23ab22380aa6aa9776b9e
Moksina-Akter <msdiya01865@gmail.com>
2025-09-08 08:22:17 +0600

    some functionalities added

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
-
 const catagoryContainer = document.getElementById('catagoryContainer');
 const plantCardContainer = document.getElementById('card-container');
+const cartContainer = document.getElementById('cartContainer');
+
+let cart = [];
 
 //alltrees
 const allTrees = () =>{
      fetch('https://openapi.programming-hero.com/api/plants')
     .then((response) => response.json())
     .then((data) => {
-        // console.log(data);
-        // const categories = data.categories;
         showAllTrees(data.plants) ;   
     })
     .catch(err => {
@@ -22,7 +22,7 @@ const allTrees = () =>{
 const showAllTrees = (allTrees)=>{
     //   console.log(allTrees);
       plantCardContainer.innerHTML ='';
-      allTrees.forEach( tree => {
+       allTrees.forEach( tree => {
        plantCardContainer.innerHTML += `
            <div class="p-4 space-y-2 bg-white rounded-md shadow-lg">
                  <div class="h-[220px]">
@@ -38,7 +38,6 @@ const showAllTrees = (allTrees)=>{
              </div>
         `;
    })
-  
 }
 
 //loadCategories-part
@@ -108,11 +107,55 @@ const plantCategory = (cardId) =>{
                     <h2 class="bg-[#dcfce7] text-[#15803d] py-1 px-2 rounded-2xl">${plant.category}</h2>
                    <h3>৳<span class="text-[#15803d]">${plant.price}</span></h3>
                </div>
-              <button class="bg-[#15803d] w-full py-1 px-2 rounded-3xl text-white">Add to Cart</button>      
+              <button class="btn bg-[#15803d] w-full py-1 px-2 rounded-3xl text-white">Add to Cart</button>      
              </div>
         `;
    })
   }
+
+
+ //cart part
+plantCardContainer.addEventListener('click', (e) => {
+    if (e.target.innerText === 'Add to Cart') {
+        handleCart(e);
+    }
+});
+
+const handleCart = (e) => {
+    const name = e.target.parentNode.childNodes[3].innerText;
+    const price = e.target.parentNode.childNodes[7].childNodes[3].innerText;
+    const id = e.target.parentNode.id;
+    console.log(id)
+   
+        cart.push({
+          name: name,
+          price: price
+        })
+        showCart(cart);
+}
+
+//show cart
+const showCart = (carts) => {
+   cartContainer.innerHTML = '';
+   carts.forEach ( cart =>{
+     cartContainer.innerHTML += `
+        <div class="bg-[#F0FDF4] mt-4 p-2 rounded flex justify-between items-center">
+            <div> 
+                <h1 class="font-bold text-xl">${cart.name}</h1>
+                <p>${cart.price}</p>
+            </div>
+            <button onclick="handleDelete('${cart.id}')" class="text-red-700">❌</button>
+        </div>
+
+     `;
+   })
+}
+
+//handle delete part
+const handleDelete = (deleteId) => {
+   const filtered = cart.filter(cart => cart.id !== deleteId);
+   cart = filtered;
+}
 
 
 loadCategories();
